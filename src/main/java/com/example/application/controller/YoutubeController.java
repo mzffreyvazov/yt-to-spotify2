efe55001c7cae7a569e4daa54836ed858b411c0a
mzffreyvazov <mzffreyvazov2005@gmail.com>
2025-06-03 23:18:20 +0400

    Refactor: Clean up .dockerignore and add spacing in YoutubeController for improved readability

--- src/main/java/com/example/application/controller/YoutubeController.java
+++ src/main/java/com/example/application/controller/YoutubeController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class YoutubeController {
     
     private YoutubeService youtubeService;
+    
     public YoutubeController(YoutubeService youtubeService) {
         this.youtubeService = youtubeService;
     }
