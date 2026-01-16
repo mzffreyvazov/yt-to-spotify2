95490ed9db2d30b9b112e993b66b2f0c87fc1808
mzffreyvazov <mzffreyvazov2005@gmail.com>
2026-01-17 01:04:40 +0400

    update

--- src/main/java/com/example/application/config/AppShell.java
+++ src/main/java/com/example/application/config/AppShell.java
@@ -2,11 +2,9 @@ package com.example.application.config;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.component.page.Push;
-import com.vaadin.flow.theme.Theme;
 
 
 @Push 
-@Theme("default")
 public class AppShell implements AppShellConfigurator {
     
 }
